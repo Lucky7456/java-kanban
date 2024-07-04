a37602f5be0ddc46fb2cdc9c5f70f992d9ca2e8f
Lucky7456 <amir.abdrahmanov@yandex.ru>
2024-07-04 19:03:34 +0500

    feat: sprint 6 optional * Main.java user scenario testing

--- src/Main.java
+++ src/Main.java
@@ -18,60 +18,56 @@ public class Main {
 
         SubTask st1 = new SubTask("first st", "sub 1", TaskStatus.NEW);
         SubTask st2 = new SubTask("second st", "sub 2", TaskStatus.NEW);
-        EpicTask et1 = new EpicTask("first epic", st1.getName() + "; " + st2.getName());
+        SubTask st3 = new SubTask("third st", "sub 3", TaskStatus.NEW);
+        EpicTask et1 = new EpicTask("first epic", "epic with 3 subs");
         st1.setEpicTask(et1);
         st2.setEpicTask(et1);
+        st3.setEpicTask(et1);
         et1.addSubTask(st1);
         et1.addSubTask(st2);
+        et1.addSubTask(st3);
 
         tm.createSubTask(st1);
         tm.createSubTask(st2);
+        tm.createSubTask(st3);
         tm.createEpicTask(et1);
 
-        SubTask st3 = new SubTask("third st", "sub 3", TaskStatus.NEW);
-        EpicTask et2 = new EpicTask("second epic", st3.getName());
-        st3.setEpicTask(et2);
-        et2.addSubTask(st3);
+        EpicTask et2 = new EpicTask("second epic", "empty epic");
 
-        tm.createSubTask(st3);
         tm.createEpicTask(et2);
 
         System.out.println("___________________");
         System.out.println("tasks:\n");
         printAllTasks(tm);
-
-
-        tm.updateTask(new Task("first", "to do", t1.getId(), TaskStatus.DONE));
-        tm.updateTask(new Task("second", "to do", t2.getId(), TaskStatus.IN_PROGRESS));
-        tm.updateSubTask(new SubTask("second st", "sub 2", st2.getId(), TaskStatus.IN_PROGRESS));
-        tm.updateSubTask(new SubTask("first st", "sub 1", st1.getId(), TaskStatus.DONE));
-        tm.updateSubTask(new SubTask("third st", "sub 3", st3.getId(), TaskStatus.DONE));
-        tm.updateEpicTask(new EpicTask("first epic", st1.getName() + "; " + st2.getName(), et1.getId()));
+        System.out.println("___________________\n");
 
         tm.getSubTaskById(st3.getId());
+        printHistory(tm);
         tm.getTaskById(t1.getId());
+        printHistory(tm);
         tm.getEpicTaskById(et2.getId());
+        printHistory(tm);
         tm.getTaskById(t2.getId());
+        printHistory(tm);
         tm.getTaskById(t1.getId());
-
-        System.out.println("\n___________________");
-        System.out.println("updated tasks:");
-        printAllTasks(tm);
-
-        tm.getEpicTaskById(et1.getId());
-        tm.getSubTaskById(st1.getId());
-        tm.getSubTaskById(st2.getId());
-        tm.getSubTaskById(st1.getId());
+        printHistory(tm);
         tm.getEpicTaskById(et2.getId());
+        printHistory(tm);
+        tm.getEpicTaskById(et1.getId());
+        printHistory(tm);
+        tm.getSubTaskById(st1.getId());
+        printHistory(tm);
+        tm.getSubTaskById(st2.getId());
+        printHistory(tm);
         tm.getTaskById(t1.getId());
+        printHistory(tm);
+        tm.getSubTaskById(st3.getId());
+        printHistory(tm);
 
-        tm.removeTaskById(t2.getId());
-        tm.removeEpicTaskById(et2.getId());
-
-        System.out.println("\n___________________");
-        System.out.println("after removing tasks:");
-        printAllTasks(tm);
-
+        tm.removeSubTaskById(st2.getId());
+        printHistory(tm);
+        tm.removeEpicTaskById(et1.getId());
+        printHistory(tm);
     }
 
     private static void printAllTasks(TaskManager manager) {
@@ -92,6 +88,10 @@ public class Main {
             System.out.println(subtask);
         }
 
+        printHistory(manager);
+    }
+
+    private static void printHistory(TaskManager manager) {
         System.out.println("История:");
         for (Task task : manager.getHistory()) {
             System.out.println(task);
