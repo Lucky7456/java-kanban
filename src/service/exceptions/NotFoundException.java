5f700867e50f850edaa53b2bdc4f1c785f02835f
Lucky7456 <amir.abdrahmanov@yandex.ru>
2024-08-29 01:09:00 +0500

    fix: sprint 9 main tor * codestyle

--- src/service/exceptions/NotFoundException.java
+++ src/service/exceptions/NotFoundException.java
@@ -1,6 +1,6 @@
 package service.exceptions;
 
-public class NotFoundException extends RuntimeException{
+public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
     }
